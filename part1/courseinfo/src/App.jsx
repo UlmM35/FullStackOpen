39c0b16ddf8467753f104d3d316aff4af4873fe9
UlmM35 <125765792+UlmM35@users.noreply.github.com>
2025-07-24 14:29:43 +0300

    Completed 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,61 @@
+const Header = (course) => {
+  return (
+    <>
+      <h1>{course.name}</h1>
+    </>
+  )
+}
+
+const Content = (count) => {
+  return (
+    <>
+      <Part part={count.part1} exercise={count.exercise1}/>
+      <Part part={count.part2} exercise={count.exercise2}/>
+      <Part part={count.part3} exercise={count.exercise3}/>
+    </>
+  )
+}
+
+const Part = (content) => {
+  return (
+    <>
+      <p>{content.part} {content.exercise}</p>
+    </>
+  )
+}
+
+const Total = (number) => {
+  return (
+    <> 
+      <p>Number of excercises {number.first + number.second + number.third}</p>
+    </>
+  )
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course.name}/>
+      <Content part1={course.parts[0].name} part2={course.parts[1].name} part3={course.parts[2].name} exercise1={course.parts[0].exercises} exercise2={course.parts[1].exercises} exercise3={course.parts[2].exercises}/>
+      <Total first={course.parts[0].exercises} second={course.parts[1].exercises} third={course.parts[2].exercises}/>
     </div>
   )
 }
