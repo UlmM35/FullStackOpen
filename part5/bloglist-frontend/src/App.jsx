2f2456a868f78774e55784a6be35e3732368cec1
UlmM35 <125765792+UlmM35@users.noreply.github.com>
2025-08-11 14:49:45 +0300

    Completed 5.3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,19 +1,111 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [blogs])
+
+  useEffect(() => {
+    const loggedUserJson = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJson) {
+      const user = JSON.parse(loggedUserJson)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
+  const handleLogOut = (event) => {
+    event.preventDefault()
+    window.localStorage.clear()
+  }
+
+  const handleCreate = async (event) => {
+    event.preventDefault()
+
+    const newBlog = {
+      title: `${title}`,
+      author: `${author}`,
+      url: `${url}`
+    }
+    try {
+      const response = await blogService.create(newBlog)
+    } catch (exception){
+      console.log(exception)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+              <input
+              type='text'
+              value={username}
+              name='Username'
+              onChange={({ target }) => setUsername(target.value)}
+              />
+          </div>
+          <div>
+            password
+              <input
+              type='password'
+              value={password}
+              name='Password'
+              onChange={({ target }) => setPassword(target.value)}
+              />
+          </div>
+          <button type='submit'>login</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div>
       <h2>blogs</h2>
+      <div>{user.name} logged in <button onClick={handleLogOut}>log out</button></div>
+      <h2>create new blogs</h2>
+      <form onSubmit={handleCreate}>
+        <div>title: <input type='text' value={title} name='Title' onChange={({ target }) => setTitle(target.value)}/></div>
+        <div>author: <input type='text' value={author} name='Author' onChange={({ target }) => setAuthor(target.value)}/></div>
+        <div>url: <input type='url' value={url} name='Url' onChange={({ target }) => setUrl(target.value)}/></div>
+        <button type='submit'>create</button>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
