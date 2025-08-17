7dbd0760f8923967e00d9e3790e8fdf265e4ef4e
ulm <125765792+UlmM35@users.noreply.github.com.>
2025-08-17 18:16:05 +0300

    Completed 6.8

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,11 +19,40 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+export const voteForAnecdote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: {id}
+  }
+}
 
-  return state
+export const createAnecdote = (anecdote) =>{
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: {
+      content: anecdote,
+      id: getId(),
+      votes: 0
+    }
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.payload.id
+      const anecdoteToChange = state.find(a => a.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(a => a.id !== id ? a : changedAnecdote)
+    }
+    case 'NEW_ANECDOTE':
+      return [...state, action.payload]
+    default: 
+      return state
+  }
 }
 
 export default reducer
