3b9996177395be42ec13eb61909e23f8061ada86
ulm <125765792+UlmM35@users.noreply.github.com.>
2025-08-29 14:08:33 +0300

    Completed 9.2

--- part9/ts-exercises/exerciseCalculator.ts
+++ part9/ts-exercises/exerciseCalculator.ts
@@ -1,6 +1,6 @@
 interface Result {
     periodLength: number;
-    trainingDays: 5;
+    trainingDays: number;
     success: boolean;
     rating: number;
     ratingDescription: string;
@@ -8,6 +8,41 @@ interface Result {
     average: number;
 }
 
-const calculateExercises = (trainingLength: string[], target: number) {
-    const
+const calculateExercises = (trainingLength: number[], target: number): Result => {
+
+    const trainedDays: number = trainingLength.filter(day => day > 0).length
+
+    let description: string;
+
+    const calculateAverage = (days: number[]): number => {
+        return days.reduce((accum, curr) => accum + curr, 0)/days.length
+    }
+
+    const calculateRating = (days: number[], target: number): number => {
+        const avg = calculateAverage(days)
+        if (avg >= target) {
+            description = 'good'
+            return 3
+        } else if (avg < target && avg - target < 0.5) {
+            description = 'okay'
+            return 2
+        } else {
+            description = 'bad'
+            return 1
+        }
+    }
+
+    const result: Result = {
+        periodLength: trainingLength.length,
+        trainingDays: trainedDays,
+        success: trainingLength.length <= trainedDays,
+        rating: calculateRating(trainingLength, target),
+        ratingDescription: description,
+        target: target,
+        average: trainingLength.reduce((accum, curr) => accum + curr, 0)/trainingLength.length
+    }
+
+    return result
 }
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
