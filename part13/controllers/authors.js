dd118a5023ce8a48bbc9ad28b1b7277eaf959814
UlmM35 <125765792+UlmM35@users.noreply.github.com>
2025-12-13 00:31:22 +0200

    13.16 done

---
+++ part13/controllers/authors.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+const { sequelize } = require('../util/db')
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('author')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+    ],
+    group: ['author'],
+    order: [[sequelize.literal('likes'), 'DESC']]   // Bonus: order by total likes
+  })
+  
+  res.json(authors)   
+})
+
+module.exports = router
