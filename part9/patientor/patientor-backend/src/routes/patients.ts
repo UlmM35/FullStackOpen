a05771ae99aa3d10f146466c5b4ea8eea4e14245
ulm <125765792+UlmM35@users.noreply.github.com.>
2026-01-22 23:22:24 +0200

    9.14 done

--- part9/patientor/patientor-backend/src/routes/patients.ts
+++ part9/patientor/patientor-backend/src/routes/patients.ts
@@ -1,8 +1,9 @@
-import express from "express";
-import { Response } from "express";
-import { NonSensitivePatient} from "../types";
+import express, { NextFunction } from "express";
+import { Request, Response } from "express";
+import { NewPatientEntry, NonSensitivePatient, Patient } from "../types";
 import patientService from "../services/patientService";
-import { toNewPatientEntry } from "../utils";
+import { newEntrySchema } from "../utils";
+import { z } from 'zod';
 
 const router = express.Router();
 
@@ -10,18 +11,29 @@ router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
     res.send(patientService.getPatients());
 });
 
-router.post('/', (req, res) => {
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
     try {
-        const newPatientEntry = toNewPatientEntry(req.body);
-
-        const addedPatientEntry = patientService.addPatient(newPatientEntry);
-        res.json(addedPatientEntry);
+        newEntrySchema.parse(req.body);
+        console.log(req.body);
+        next();
     } catch (error: unknown) {
-        let errorMessage = 'Something went wrong :(';
-        if (error instanceof Error) {
-            errorMessage = 'Error: ' + error.message;
-        }
-        res.status(400).send(errorMessage);
+        next(error);
     }
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => { 
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+router.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatientEntry>, res: Response<Patient>) => {
+    const addedEntry = patientService.addPatient(req.body);
+    res.json(addedEntry);
 });
+
+router.use(errorMiddleware);
+
 export default router;
