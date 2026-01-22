b8aaf48594b7ba3e4095fb7e06d4b94289b21438
ulm <125765792+UlmM35@users.noreply.github.com.>
2026-01-22 20:59:33 +0200

    9.13 done

--- part9/patientor/patientor-backend/src/routes/patients.ts
+++ part9/patientor/patientor-backend/src/routes/patients.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Response } from "express";
 import { NonSensitivePatient} from "../types";
 import patientService from "../services/patientService";
+import { toNewPatientEntry } from "../utils";
 
 const router = express.Router();
 
@@ -9,4 +10,18 @@ router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
     res.send(patientService.getPatients());
 });
 
+router.post('/', (req, res) => {
+    try {
+        const newPatientEntry = toNewPatientEntry(req.body);
+
+        const addedPatientEntry = patientService.addPatient(newPatientEntry);
+        res.json(addedPatientEntry);
+    } catch (error: unknown) {
+        let errorMessage = 'Something went wrong :(';
+        if (error instanceof Error) {
+            errorMessage = 'Error: ' + error.message;
+        }
+        res.status(400).send(errorMessage);
+    }
+});
 export default router;
--- part9/patientor/patientor-backend/src/services/patientService.ts
+++ part9/patientor/patientor-backend/src/services/patientService.ts
@@ -1,12 +1,25 @@
+import { v1 as uuid } from "uuid";
 import patientData from "../../data/patients";
-import { NonSensitivePatient } from "../types";
+import { NewPatientEntry, NonSensitivePatient, Patient } from "../types";
 
-const patients: NonSensitivePatient[] = patientData;
+const patients: Patient[] = patientData;
 
 const getPatients = (): NonSensitivePatient[] => {
-    return patients;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return patients.map(({ ssn, ...rest }) => rest);
+};
+
+const addPatient = ( entry: NewPatientEntry): Patient => {
+    const newPatientEntry = {
+        id: uuid(),
+        ...entry
+    };
+
+    patients.push(newPatientEntry);
+    return newPatientEntry;
 };
 
 export default {
-    getPatients
+    getPatients,
+    addPatient
 };
